8172f6d8c9d6664177b206aea38d7a92cafbbcc3
Johnathan Irvin <irvinjohnathan@gmail.com>
2022-01-02 15:03:05 -0500

    Example plugins

---
+++ plugins/salutation_plugin.py
@@ -0,0 +1,68 @@
+# Copyright (c) 2021 Johnathan P. Irvin
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+from korth_spirit import EventEnum, Instance
+from korth_spirit.events import Event
+
+
+class SalutationPlugin:
+    """
+    Greeter plugin.
+    """
+    @property
+    def on_event(self) -> EventEnum:
+        """
+        Event to listen for.
+        """
+        return EventEnum.AW_EVENT_CHAT
+
+    def __init__(self, instance: Instance) -> None:
+        """
+        Initialize the plugin.
+        """
+        self.instance = instance
+    
+    def handle_event(self, event: Event) -> None:
+        """
+        Handle the event.
+
+        Args:
+            event (Event): The event.
+        """
+        greetings = [
+            "hello",
+            "hi",
+            "hey",
+            "greetings",
+            "salutations",
+            "bonjour",
+            "hola",
+            "bonsoir",
+            "guten tag",
+        ]
+
+        if not any(
+            event.chat_message.lower().startswith(f'{greet} {self.instance.name.lower()}')
+            for greet in greetings
+        ):
+            return
+
+        self.instance.say(f"Hello, {event.avatar_name}!")
+
