176f86e0a13c4bc5ed02947f8797626b10291fe6
Johnathan Irvin <irvinjohnathan@gmail.com>
2022-01-08 22:08:46 -0500

    Remove unused dependencies

--- plugin_bot/plugin/bus.py
+++ plugin_bot/plugin/bus.py
@@ -17,11 +17,9 @@
 # NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
 # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-from functools import wraps
 from typing import List
 
-from korth_spirit import EventEnum, Instance
+from korth_spirit import Instance
 
 from .plugin import Plugin
 
