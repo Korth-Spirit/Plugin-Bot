d12769458f9fb3f1119e89f62af9ab98dfc55f3b
Johnathan Irvin <irvinjohnathan@gmail.com>
2022-01-13 23:44:11 -0500

    Remove unused imports

--- plugins/custom_event_plugin.py
+++ plugins/custom_event_plugin.py
@@ -18,8 +18,7 @@
 # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-from korth_spirit import EventEnum, Instance
-from korth_spirit.events import Event
+from korth_spirit import Instance
 
 
 class CustomEventPlugin:
